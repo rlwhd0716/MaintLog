486cb2f11f6e943f7ce7c13b6ee3452a7f6ca1bf
Gi-Jong <rlwhd0716@gmail.com>
2026-05-22 00:07:05 +0900

    [Home] 1. 전체 화면 구현 완료 2. 날씨카드 UI 컴팩트하게 변경

--- MaintLog_Android/app/src/main/java/com/github/maintlog_android/view/home/HomeFragment.kt
+++ MaintLog_Android/app/src/main/java/com/github/maintlog_android/view/home/HomeFragment.kt
@@ -1,27 +1,40 @@
 package com.github.maintlog_android.view.home
 
-import androidx.navigation.fragment.findNavController
 import com.github.domain.model.action.ActionData
 import com.github.maintlog_android.R
+import com.github.maintlog_android.databinding.BottomSheetHomeAddBinding
 import com.github.maintlog_android.databinding.FragmentHomeBinding
 import com.github.maintlog_android.view.improvement.detail.ImprovementDetailActivity
-import com.github.maintlog_android.view.main.MainViewModel
 import com.github.maintlog_android.view.improvement.form.ImprovementFormActivity
+import com.github.maintlog_android.view.incident.form.IncidentFormActivity
+import com.github.maintlog_android.view.main.MainViewModel
 import com.github.util.base.BaseFragment
 import com.github.util.base.BaseRecyclerViewAdapter
 import com.github.util.extension.startActivityInFragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment: BaseFragment<FragmentHomeBinding, MainViewModel>(R.layout.fragment_home), BaseRecyclerViewAdapter.OnClickEvent<ActionData>{
+class HomeFragment : BaseFragment<FragmentHomeBinding, MainViewModel>(R.layout.fragment_home),
+    BaseRecyclerViewAdapter.OnClickEvent<ActionData> {
     override var bindingApply: (FragmentHomeBinding.() -> Unit)? = {
+        fabHomeAdd.setOnClickListener {
+            val bottomSheet = BottomSheetDialog(requireContext())
+            val sheetBinding = BottomSheetHomeAddBinding.inflate(layoutInflater)
 
-        /* 하단 버튼 */
-        btnHomeAddRequest.setOnClickListener {
-            startActivityInFragment<ImprovementFormActivity>()
-        }
-        btnHomeAddIncident.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_incidentFormFragment)
+            bottomSheet.setContentView(sheetBinding.root)
+
+            sheetBinding.btnAddIncident.setOnClickListener {
+                bottomSheet.dismiss()
+                startActivityInFragment<IncidentFormActivity>()
+            }
+
+            sheetBinding.btnAddImprovement.setOnClickListener {
+                bottomSheet.dismiss()
+                startActivityInFragment<ImprovementFormActivity>()
+            }
+
+            bottomSheet.show()
         }
     }
 
